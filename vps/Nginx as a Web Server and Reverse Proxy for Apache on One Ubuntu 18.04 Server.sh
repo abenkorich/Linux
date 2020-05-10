8ac0868cf6faf804ff0da02832052472eb7860e4
Abdellah Benkorich <abdellah.benkorich@gmail.com>
2020-05-10 23:36:31 +0100

    Create Nginx as a Web Server and Reverse Proxy for Apache on One Ubuntu 18.04 Server.sh

---
+++ vps/Nginx as a Web Server and Reverse Proxy for Apache on One Ubuntu 18.04 Server.sh
@@ -0,0 +1,19 @@
+  
+#!/bin/bash
+# --------------------------------------------------------------------------------------
+# Initial Date  :   10/05/2020  (dd/mm/yyyy)
+# Author        :   BENKORICH Abdellah
+# URL           :   https://www.bena.dev
+# Email         :   abdellah.benkorich@gmail.com - hello@bena.dev
+#
+# Description   :   Bash script To Configure Nginx as a Web Server and Reverse Proxy for Apache on One Ubuntu 18.04 Server
+# Usage         :   Use 'curl' to run this script directly from Github.
+# Command       :   curl -L https://gist.githubusercontent.com/abenkorich/--/raw | bash
+#
+# License       :   MIT license -> https://opensource.org/licenses/MIT
+# Tested On     :   Ubuntu 18.04
+#
+# Resources     :   https://www.digitalocean.com/community/tutorials/how-to-configure-nginx-as-a-web-server-and-reverse-proxy-for-apache-on-one-ubuntu-18-04-server
+#               :   https://www.digitalocean.com/community/tutorials/apache-vs-nginx-practical-considerations
+# --------------------------------------------------------------------------------------
+#
